4e4084c5d58d5f3aece22d8ebc8c124508fc8252
Casey Heitz & Jeff Austin <student@epicodus.com>
2015-08-19 10:47:51 -0700

    Added passing tests for save, getAll, and deleteAll

--- tests/CuisineTest.php
+++ tests/CuisineTest.php
@@ -42,5 +42,48 @@
 
             $this->assertEquals(true, is_numeric($result));
         }
+
+        function test_save()
+        {
+            // Arrange
+            $type = "Mexican";
+            $test_Cuisine = new Cuisine($type);
+            $test_Cuisine->save();
+
+            // Act
+            $result = Cuisine::getAll();
+
+            // Assert
+            $this->assertEquals($test_Cuisine, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            $type = "Mexican";
+            $type2 = "Italian";
+            $test_Cuisine = new Cuisine($type);
+            $test_Cuisine->save();
+            $test_Cuisine2 = new Cuisine($type2);
+            $test_Cuisine2->save();
+
+            $result = Cuisine::getAll();
+
+            $this->assertEquals([$test_Cuisine, $test_Cuisine2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            $type = "Mexican";
+            $type2 = "Italian";
+            $test_Cuisine = new Cuisine($type);
+            $test_Cuisine->save();
+            $test_Cuisine2 = new Cuisine($type2);
+            $test_Cuisine2->save();
+
+            Cuisine::deleteAll();
+            $result = Cuisine::getAll();
+
+            $this->assertEquals([], $result);
+        }
     }
  ?>
